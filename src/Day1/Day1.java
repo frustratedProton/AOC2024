e078dfcd6e64a13a66d94125742a478790a9a3d0
Proton <aryan29052003@gmail.com>
2024-12-14 11:54:17 +0530

    Day 1 added

--- src/Day1/Day1.java
+++ src/Day1/Day1.java
@@ -4,11 +4,13 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 
 public class Day1 {
-    public static int totalDist(Path filepath) throws IOException {
+    public static List<List<Integer>> extractLists(Path filepath) throws IOException {
         // reading input file
         List<Integer> left;
         List<Integer> right;
@@ -28,8 +30,33 @@ public class Day1 {
                     .toList();
         }
 
+        return List.of(left, right);
+    }
+
+    // Part 1
+    public static int totalDist(Path filepath) throws IOException {
+        List<List<Integer>> lists = extractLists(filepath);
+
+        List<Integer> left = lists.get(0);
+        List<Integer> right = lists.get(1);
+
         return IntStream.range(0, left.size())
                 .map(i -> Math.abs(left.get(i) - right.get(i)))
+                .sum();
+    }
+
+    // Part 2
+    public static int totalSimilarity(Path filepath) throws IOException {
+        List<List<Integer>> lists = extractLists(filepath);
+
+        List<Integer> left = lists.get(0);
+        List<Integer> right = lists.get(1);
+
+        Map<Integer, Long> rightCount = right.stream()
+                .collect(Collectors.groupingBy(num -> num, Collectors.counting()));
+
+        return left.stream()
+                .mapToInt(num -> num * rightCount.getOrDefault(num, 0L).intValue())
                 .sum();
     }
 
@@ -37,6 +64,9 @@ public class Day1 {
         Path filepath = Path.of("src/Day1/day1.txt");
 
         int totalDist = totalDist(filepath);
-        System.out.println(totalDist);
+        System.out.println("Total Distance: " + totalDist);
+
+        int similarity = totalSimilarity(filepath);
+        System.out.println("Similarity: " + similarity);
     }
 }
